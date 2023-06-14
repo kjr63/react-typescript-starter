b9cb67a4768e87b7fe8dc245a425e2b6fa1ed6f8
kjr63 <kaj.risberg@helsinkinet.fi>
2023-06-14 14:34:46 +0300

    v1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
-import HelloWorld from "components/HelloWorld";
+import HelloWorld from "./components/HelloWorld";
+import Hello from "./components/Hello";
+import HelloClass from "./components/HelloClass";
+import HelloState from "./components/HelloState";
 
-const App: React.FC = () => <HelloWorld />;
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <HelloWorld />
+            <Hello name="Liisa" enthusiasmLevel={3}></Hello>
+            <HelloClass name="Liisa" enthusiasmLevel={1}></HelloClass>
+            <HelloState name="Elvis" enthusiasmLevel={2}></HelloState>
+        </div>
+    );
+};
 
 /* function App(): React.FC {
     return <HelloWorld />;
